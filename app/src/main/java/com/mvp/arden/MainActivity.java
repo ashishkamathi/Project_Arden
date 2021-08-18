8858f380ef91886da78f08ba6be38b31edef91de
ashishkamathi <32205608+ashishkamathi@users.noreply.github.com>
2021-08-18 20:42:03 +0530

    Initial Commit

---
+++ app/src/main/java/com/mvp/arden/MainActivity.java
@@ -0,0 +1,18 @@
+package com.mvp.arden;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
